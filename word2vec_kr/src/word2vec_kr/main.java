080c40e0c71710175113aa9ad7a474f79b78c6b9
Sergey Leksikov <lexikovs@gmail.com>
2020-05-20 20:42:02 +0900

    packages

--- word2vec_kr/src/word2vec_kr/main.java
+++ word2vec_kr/src/word2vec_kr/main.java
@@ -3,6 +3,13 @@
  */
 package word2vec_kr;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 /**
  * @author lexik
  *
@@ -11,11 +18,17 @@ public class main {
 
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		System.out.println("Hello word2vec");
-		
+		Document doc = Jsoup.connect("https://en.wikipedia.org/").get();
+		System.out.println(doc.title());
+		Elements newsHeadlines = doc.select("#mp-itn b a");
+		for (Element headline : newsHeadlines) {
+			System.out.printf("%s\n\t%s", headline.attr("title"), headline.absUrl("href")); 
+		}
 	}
 
 }
