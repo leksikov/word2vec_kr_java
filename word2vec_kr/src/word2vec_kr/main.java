1c12ddf00098d96a347c2c04f07c81461b056e59
Sergey Leksikov <lexikovs@gmail.com>
2020-05-20 21:02:53 +0900

    Setting crawler basics
    Setting crawler basics

--- word2vec_kr/src/word2vec_kr/main.java
+++ word2vec_kr/src/word2vec_kr/main.java
@@ -9,7 +9,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
+import crawler.Crawler;
 /**
  * @author lexik
  *
@@ -23,12 +23,11 @@ public class main {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		System.out.println("Hello word2vec");
-		Document doc = Jsoup.connect("https://en.wikipedia.org/").get();
-		System.out.println(doc.title());
-		Elements newsHeadlines = doc.select("#mp-itn b a");
-		for (Element headline : newsHeadlines) {
-			System.out.printf("%s\n\t%s", headline.attr("title"), headline.absUrl("href")); 
-		}
+		Crawler cr = new Crawler();
+		System.out.println(cr.getHeadline());
+		
+		
 	}
+
 
 }
